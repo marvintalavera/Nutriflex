2d3440bd64a598c2b450fb86e68b173e3e7a66d4
marvintalavera <talavera.marvin@rocketmail.com>
2023-09-09 11:20:25 -0700

    .

---
+++ notes.js
@@ -0,0 +1,19 @@
+//day 1 (9-9-23)
+
+//first commit
+
+//currently added backend and frontend folder
+
+//the backend is currently on port 4000 and the backend is on port 3000
+
+//will change that soon but going to commit because it works as of now
+
+
+
+//second commit
+
+//added the middleware and now the express app is setup with the barebones
+
+
+
+//third commit
